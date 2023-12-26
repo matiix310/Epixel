c36cfddacd2909c2472e2dc16f16ffce202ea224
Lucas Stephan <lucas.stephan@epita.fr>
2023-12-26 01:43:14 +0100

    fix: types 5

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -24,7 +24,7 @@ export const options: NextAuthOptions = {
             data: {
               username: profile.name as string,
               password: "",
-              createdAt: Date.now().toString(),
+              createdAt: Date.now(),
             },
           });
         }
