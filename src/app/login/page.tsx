e56f3b3a0385327cf96d754fe4c15ce577f35323
Lucas Stephan <lucas.stephan@epita.fr>
2023-12-28 00:46:40 +0100

    remove debug console.log

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,8 +8,6 @@ export default async function loginChecker({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  console.log(process.env);
-
   const session = await getServerSession(authOptions);
 
   const callbackUrl = (searchParams.callbackUrl ?? "/") as string;
